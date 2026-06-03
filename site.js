3a408024d7837141b1e3829f1adad8de07b31a83
dustin5mueller87-stack <dustin5mueller87-stack@users.noreply.github.com>
2026-06-04 00:36:47 +0200

    feat: integrate Voiceflow chat widget globally

--- site.js
+++ site.js
@@ -119,3 +119,6 @@
     init();
   }
 })();
+
+/* Voiceflow Chat Widget Integration */
+(function(d,t){var v=d.createElement(t),s=d.getElementsByTagName(t)[0];v.onload=function(){window.voiceflow.chat.load({verify:{projectID:'6a0977f2a62d285256e0577a'},url:'https://general-runtime.voiceflow.com',voice:{url:'https://runtime-api.voiceflow.com'}})};v.src='https://cdn.voiceflow.com/widget-next/bundle.mjs';v.type='text/javascript';s.parentNode.insertBefore(v,s)})(document,'script');
